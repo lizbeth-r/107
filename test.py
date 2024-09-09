c562efc02423926d1510d3166f5525cabad23398
Kale e <kalee@Kales-MacBook-Pro.local>
2024-09-09 13:49:54 -0700

    Assignment 1

---
+++ test.py
@@ -0,0 +1,39 @@
+print("Hello from python")
+
+variable = 1
+variiable = True
+variable = "Hello"
+
+array = [1, 2, 3, 4, 5]
+print(array[4])
+array.append(6)
+print(array)
+array.pop()
+print(array)
+
+dict = {
+    "name" : "Lizbeth",
+    "last_name" : "Ramirez"
+}
+
+print(dict)
+print(dict["name"])
+
+#for
+
+for numbers in array:
+    print("Lizbeth")
+
+for i in dict:
+    print(dict[i])
+
+#fuction print(){}
+
+def hello():
+    print("hello from python fuction")
+
+hello()
+
+
+
+
