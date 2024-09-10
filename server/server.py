bd4406e92c5d4cd461ea9d986b1c4a6f4b9af38f
Kale e <kalee@Kales-MacBook-Pro.local>
2024-09-09 20:54:18 -0700

    Assignment 3

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
@@ -16,5 +16,32 @@ def about():
 def footer():
     pageName = {"pageName": "organika"}
     return json.dumps(pageName)
+
+products = []
+
+@app.get("/api/products")
+def read_products():
+    return json.dumps(products)
+
+@app.post("/api/products")
+def save_products():
+    item = request.get_json()
+    products.append(item)
+    print(item)
+    return json.dumps(item)
+
+@app.put("/api/products/<int:index>")
+def update_products(index):
+    update_item = request.get_json()
+    if 0<=index<len(products):
+        products[index]=update_item
+        return json.dumps(update_item)
+    else:
+        return "That index does not exist"
+    
+@app.get("/api/products/count")
+def num_products():
+    count = len(products)
+    return json.dumps({"count": count})
 
 app.run(debug=True)
