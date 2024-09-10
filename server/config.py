5e2c46b6163ab3143b8e5636ec8ba22e1b9013d5
Kale e <kalee@Kales-MacBook-Pro.local>
2024-09-10 15:57:27 -0700

    Final Report

--- server/config.py
+++ server/config.py
@@ -2,5 +2,5 @@ import pymongo
 import certifi
 con_str = "mongodb+srv://aglaeramirez266:kale123@cluster0.kjacf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-client = pymongo.MongoClient(con_str, tlsCAfile=certifi.where())
+client = pymongo.MongoClient(con_str, tlsCAFile=certifi.where())
 db=client.get_database("organika")
--- server/server.py
+++ server/server.py
@@ -1,3 +1,4 @@
+from flask import Flask
 from flask import Flask, request
 import json
 from config import db
@@ -6,7 +7,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "hello from flask"
+    return "Welcome to the product catalog"
 
 @app.get("/about")
 def about():
@@ -24,30 +25,54 @@ def fix_id(obj):
     obj["_id"]=str(obj["_id"])
     return obj
 
-@app.get("/api/products")
-def read_products():
-    return json.dumps(products)
+@app.get("/api/catalog")
+def get_catalog():
+    products = list(db.products.find())
+    return json.dumps([fix_id(product) for product in products])
 
-@app.post("/api/products")
-def save_products():
+#  Los datos se gaurdaran en la base de datos
+# @app.get("/api/products")
+# def read_products():
+#     return json.dumps(products)
+
+@app.post("/api/catalog")
+def save_product():
     item = request.get_json()
-    # products.append(item)
     db.products.insert_one(item)
-    print(item)
     return json.dumps(fix_id(item))
 
-@app.put("/api/products/<int:index>")
-def update_products(index):
-    update_item = request.get_json()
-    if 0<=index<len(products):
-        products[index]=update_item
-        return json.dumps(update_item)
-    else:
-        return "That index does not exist"
+# @app.post("/api/products")
+# def save_products():
+#     item = request.get_json()
+#     # products.append(item)
+#     db.products.insert_one(item)
+#     print(item)
+#     return json.dumps(fix_id(item))
+
+#  Los datos se actualizaran en la base de datos
+# @app.put("/api/products/<int:index>")
+# def update_products(index):
+#     update_item = request.get_json()
+#     if 0<=index<len(products):
+#         products[index]=update_item
+#         return json.dumps(update_item)
+#     else:
+#         return "That index does not exist"
+    
+@app.get("/api/reports/total")
+def get_total_value():
+    products = list(db.products.find())
+    total_value = sum(product.get('price', 0) for product in products)
+    return json.dumps({"total_value": total_value})
+
+@app.get("/api/products/<category>")
+def get_products_by_category(category):
+    products = list(db.products.find({"category": category}))
+    return json.dumps([fix_id(product) for product in products])
     
 @app.get("/api/products/count")
 def num_products():
-    count = len(products)
+    count = db.products.count_documents({})
     return json.dumps({"count": count})
 
 app.run(debug=True)
